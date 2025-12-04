d5713f3441f021e72610e1865a037cde8a65edb5
vityaded <48553342+vityaded@users.noreply.github.com>
2025-12-04 10:42:37 +0200

    Migrate server to ESM for nanoid

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const { nanoid } = require('nanoid');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import { nanoid } from 'nanoid';
 
 const app = express();
 const server = http.createServer(app);
